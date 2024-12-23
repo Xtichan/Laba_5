0a1410aa2381b515b64545110ab2df3372b7015d
Xtichan <jk.ivanof@gmail.com>
2024-12-23 19:47:37 +0300

    Create SODoer.java

---
+++ Reflection/src/main/java/org/example/SODoer.java
@@ -0,0 +1,9 @@
+package org.example;
+/**
+ * SODoer является классом, реализующий интерфейс SomeOtherInterface
+ */
+public class SODoer implements SomeOtherInterface{
+    public void doSomething(){
+        System.out.println("C");
+    }
+}
