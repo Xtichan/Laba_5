fe4b936cb7fbbfc8960476721025023610908b65
Xtichan <jk.ivanof@gmail.com>
2024-12-23 19:46:55 +0300

    Create OtherImpl.java

---
+++ Reflection/src/main/java/org/example/OtherImpl.java
@@ -0,0 +1,9 @@
+package org.example;
+/**
+ * OtherImpl является классом, реализующий интерфейс SomeInterface
+ */
+public class OtherImpl implements SomeInterface{
+    public void doSomething(){
+        System.out.println("B");
+    }
+}
