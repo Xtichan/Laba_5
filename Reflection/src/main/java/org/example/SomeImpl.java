a52894ec596f6014db06fb0c5b73dac8a368ec15
Xtichan <jk.ivanof@gmail.com>
2024-12-23 19:48:32 +0300

    Create SomeImpl.java

---
+++ Reflection/src/main/java/org/example/SomeImpl.java
@@ -0,0 +1,9 @@
+package org.example;
+/**
+ * SomeImpl является классом, реализующий интерфейс SomeInterface
+ */
+public class SomeImpl implements SomeInterface{
+    public void doSomething(){
+        System.out.println("A");
+    }
+}
